896da421dfa1b424169e3d560e3203554b4ea42f
Pascal Louwes <pascal@recoveryarea.nl>
2021-02-15 11:23:35 +0100

    awaitnode now uses observers mutationobserver

--- src/components/observers.js
+++ src/components/observers.js
@@ -1,16 +1,16 @@
 const ra_observers = function (logger) {
 
 	return {
-		observeMutations: function (params) {
+		observeMutations: function (parameters) {
 
-			logger.info("observers: observeMutations", params);
+			logger.info("observers: observeMutations", parameters);
 			let found = false;
 
 			const handleMutation = function (mutation) {
 				let nodeList = [],
-					parent = params.parent,
-					children = parent.querySelectorAll(params.child);
-				const getNodeList = nodes => Array.from(nodes).filter(node => node.nodeType !== 3 && node.nodeType !== 8 && node.localName.toLowerCase() === params.child.match(/^([a-z]*)[^\.\#[]/)[0].toLowerCase());// we are only interested in nodes with the same tag name.
+					parent = parameters.parent,
+					children = parent.querySelectorAll(parameters.child);
+				const getNodeList = nodes => Array.from(nodes).filter(node => node.nodeType !== 3 && node.nodeType !== 8 && node.localName.toLowerCase() === parameters.child.match(/^([a-z]*)[^\.\#[]/)[0].toLowerCase());// we are only interested in nodes with the same tag name.
 				const getNode = (haystack, needle) => Array.from(haystack).includes(needle);// does the children collection contain the mutation target?
 
 				const handleChildList = mutation => {
@@ -19,32 +19,32 @@ const ra_observers = function (logger) {
 						for (let node of nodeList) {
 							found = true;
 							logger.log("observers: observeMutations: added element:", node);
-							node.classList.add(params.foundClass);
-							params.callback(node);
+							node.classList.add(parameters.foundClass);
+							parameters.callback(node);
 						}
 					} else if (mutation.removedNodes.length) {
 						nodeList = getNodeList(mutation.removedNodes);
 						for (let node of nodeList) {
 							found = true;
 							logger.log("observers: observeMutations: removed element:", node);
-							params.callback(node);
+							parameters.callback(node);
 						}
 					}
 
 				};
 				const handleCharacterData = mutation => {
 					const mtParent = mutation.target.parentElement;
-					if (getNode(children, mtParent) && mtParent.nodeName.toLowerCase() === params.child) {
+					if (getNode(children, mtParent) && mtParent.nodeName.toLowerCase() === parameters.child) {
 						found = true;
 						logger.log("observers: observeMutations: element characterData changed from " + mutation.oldValue + " to " + mtParent.nodeValue, mutation);
-						params.callback(mutation.target);
+						parameters.callback(mutation.target);
 					}
 				};
 				const handleAttributes = mutation => {
 					if (getNode(children, mutation.target)) {
 						found = true;
-						logger.log("observers: observeMutations: element " + mutation.attributeName + " attribute changed from " + mutation.oldValue + " to " + mutation.target.getAttribute(params.attributeName));
-						params.callback(mutation.target);
+						logger.log("observers: observeMutations: element " + mutation.attributeName + " attribute changed from " + mutation.oldValue + " to " + mutation.target.getAttribute(parameters.attributeName));
+						parameters.callback(mutation.target);
 					}
 				};
 
@@ -71,13 +71,13 @@ const ra_observers = function (logger) {
 						logger.error("observers: observeMutations: error caught", error);
 					}
 				});
-				if (found && params.disconnect) {
+				if (found && parameters.disconnect) {
 					logger.log("observers: observeMutations: disconnecting, goodbye.");
 					observer.disconnect();
 				}
 			});
 
-			observer.observe(params.parent, params.config);
+			observer.observe(parameters.parent, parameters.config);
 		},
 		observeIntersections: function (element) {
 
